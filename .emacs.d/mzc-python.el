8d884d480b428186a8d7ac174ffd9be2b791a387
Max Z. Chao <maxzch@gmail.com>
2011-04-12 15:02:36 +1000

    Update .emacs.d/mzc-python.el
    Config pymacs and rope

--- .emacs.d/mzc-python.el
+++ .emacs.d/mzc-python.el
@@ -1,28 +1,29 @@
+;;;---------------------------------------------------------------
+;;; Pymacs
+;;;---------------------------------------------------------------
+(autoload 'pymacs-apply "pymacs")
+(autoload 'pymacs-call "pymacs")
+(autoload 'pymacs-eval "pymacs" nil t)
+(autoload 'pymacs-exec "pymacs" nil t)
+(autoload 'pymacs-load "pymacs" nil t)
+
+;;;---------------------------------------------------------------
+;;; Rope
+;;;---------------------------------------------------------------
+(pymacs-load "ropemacs" "rope-")
+(setq ropemacs-enable-autoimport t)
+
+;;;---------------------------------------------------------------
+;;; Python-mode
+;;;---------------------------------------------------------------
 (autoload 'python-mode "python-mode" "Python Mode." t)
 (add-to-list 'auto-mode-alist '("\\.py\\'" . python-mode))
 (add-to-list 'interpreter-mode-alist '("python" . python-mode))
+(require 'python-mode)
+(add-hook 'python-mode-hook
+	  (lambda ()
+	    (set-variable 'py-indent-offset 4)
+	    (set-variable 'py-smart-indentation nil)
+	    (set-variable 'indent-tabs-mode nil)
+	    (define-key py-mode-map "\C-m" 'newline-and-indent)))
 
-;; ;;;---------------------------------------------------------------
-;; ;;; python
-;; ;;;---------------------------------------------------------------
-;; (add-hook 'python-mode-hook
-;;      (lambda ()
-;;       (define-key python-mode-map "\"" 'electric-pair)
-;;       (define-key python-mode-map "\'" 'electric-pair)
-;;       (define-key python-mode-map "(" 'electric-pair)
-;;       (define-key python-mode-map "[" 'electric-pair)
-;;       (define-key python-mode-map "{" 'electric-pair)))
-;; (defun electric-pair ()
-;;   "Insert character pair without sournding spaces"
-;;   (interactive)
-;;   (let (parens-require-spaces)
-;;     (insert-pair)))
-
-;; ;;; bind RET to py-newline-and-indent
-;; (add-hook 'python-mode-hook '(lambda () 
-;;      (define-key python-mode-map "\C-m" 'newline-and-indent)))
-
-;; (setq-default indent-tabs-mode nil)
-;; (setq-default tab-width 4)
-
-;; (setq-default py-indent-offset 4)
