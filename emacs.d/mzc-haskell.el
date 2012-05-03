f3f7c8f48b496c080ceda5efa1d19d63d006f8c5
Max Z. Chao <max.z.chao@gmail.com>
2012-05-03 11:39:13 +1000

    Using haskell-mode from github instead of pre-installed
    Update emacs.d/mzc-haskell.el to change load path
    Update .gitignore to ignore emacs.d/vendor/haskell-mode
    Update emacs.d/vendor/repos to track haskell-mode from github

--- emacs.d/mzc-haskell.el
+++ emacs.d/mzc-haskell.el
@@ -1,5 +1,5 @@
 ;;; Load my local path
-(add-to-list 'load-path "/usr/share/emacs/site-lisp/haskell-mode")
+(add-to-list 'load-path "~/.emacs.d/vendor/haskell-mode")
 
 (require 'haskell-mode)
 (add-to-list 'auto-mode-alist '("\.hs$" . haskell-mode))
