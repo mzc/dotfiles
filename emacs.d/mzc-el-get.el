d838921c9990eb97a6adba565bd4cd3481933a8b
Max Z. Chao <max.z.chao@gmail.com>
2012-06-01 13:56:39 +1000

    emacs: install el-get
    New mzc-el-get to install and config el-get
    Update .gitignore to ignore el-get install directory
    Update init.el to invoke mzc-el-get

---
+++ emacs.d/mzc-el-get.el
@@ -0,0 +1,9 @@
+;;; Load my local path
+(add-to-list 'load-path "~/.emacs.d/el-get/el-get")
+
+(unless 
+    (require 'el-get nil t)
+  (url-retrieve "https://raw.github.com/dimitri/el-get/master/el-get-install.el"
+		(lambda (s) 
+		  (goto-char (point-max))
+		  (eval-print-last-sexp))))
