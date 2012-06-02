5c5876935250c30d1d98f19ee0daa54fe709196b
Max Z. Chao <max.z.chao@gmail.com>
2012-06-02 21:30:26 +1000

    emacs/ecb: remove unused code

--- emacs.d/mzc-ecb.el
+++ emacs.d/mzc-ecb.el
@@ -7,20 +7,11 @@
 (setq ecb-auto-compatibility-check nil)
 (setq ecb-layout-name "mzc")
 
-;(setq ecb-show-source-in-directories-buffer t)
-;(setq ecb-use-speedbar-instead-native-tree-buffer 'dir)
-
 ;; Activate/deactivate ECB
-(global-set-key [f11] 'ecb-activate-deactivate)
+(global-set-key [f11] 'ecb-minor-mode)
 
 ;; Jump between ecb windows
 (global-set-key (kbd "C-c e") 'ecb-goto-window-edit-by-smart-selection)
 (global-set-key (kbd "C-c m") 'ecb-goto-window-methods)
 (global-set-key (kbd "C-c a") 'ecb-goto-window-analyse)
 (global-set-key (kbd "C-c h") 'ecb-goto-window-history)
-
-(defun ecb-activate-deactivate ()
-  (interactive)
-  (if ecb-minor-mode
-      (ecb-deactivate)
-    (ecb-activate)))
