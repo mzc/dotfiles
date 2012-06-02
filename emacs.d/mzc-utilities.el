bdefd358eda328d25190db5bf1ff3e4f733b37b7
Max Z. Chao <max.z.chao@gmail.com>
2012-06-02 16:17:27 +1000

    emacs/utilities: remove: using '%' match parenthesis

--- emacs.d/mzc-utilities.el
+++ emacs.d/mzc-utilities.el
@@ -1,16 +1,4 @@
 ;;;---------------------------------------------------------------
-;;; Using '%' match parenthesis
-;;;---------------------------------------------------------------
-(global-set-key "%" 'match-paren)
-
-(defun match-paren (arg)
-  "Go to the matching paren if on a paren; otherwise insert %."
-  (interactive "p")
-  (cond ((looking-at "\\s\(") (forward-list 1) (backward-char 1))
-	((looking-at "\\s\)") (forward-char 1) (backward-list 1))
-	(t (self-insert-command (or arg 1)))))
-
-;;;---------------------------------------------------------------
 ;;; Easy way to record register
 ;;;---------------------------------------------------------------
 (global-set-key (kbd "C-.") 'mzc-point-to-register)
