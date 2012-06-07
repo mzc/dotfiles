d541fd112fff2ba657f67a76857d46c2a151aee0
Max Z. Chao <max.z.chao@gmail.com>
2012-06-07 16:13:34 +1000

    emacs/ediff: set window horizontally as default

--- emacs.d/mzc-ediff.el
+++ emacs.d/mzc-ediff.el
@@ -2,6 +2,4 @@
 (setq ediff-window-setup-function 'ediff-setup-windows-plain)
 
 ;; split the window horizontally or vertically depending on the frame width
-(setq ediff-split-window-function (if (> (frame-width) 150)
-				      'split-window-horizontally
-				    'split-window-vertically))
+(setq ediff-split-window-function 'split-window-horizontally)
