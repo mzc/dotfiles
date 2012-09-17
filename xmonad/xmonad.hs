8f39e4ccbeca39ccfd107bbb8b47954244e32c45
Zhen (Max) Chao <max.z.chao@gmail.com>
2012-09-17 13:00:30 +1000

    xmonad: format xmonad.hs

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -101,63 +101,72 @@ myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
     , ((modm,               xK_p     ), spawn "exe=`dmenu_path | dmenu` && eval \"exec $exe\"")
     , ((modm .|. shiftMask, xK_p     ), spawn "gmrun")
     , ((modm .|. shiftMask, xK_c     ), kill)
-    , ((modm,               xK_space ), sendMessage NextLayout)
- 
-    --  Reset the layouts on the current workspace to default
-    , ((modm .|. shiftMask, xK_space ), setLayout $ XMonad.layoutHook conf)
+    , ((modm,               xK_space ), sendMessage NextLayout)]
+
+    ++
+
+    -- Handle layouts of workspace
+    [ ((modm .|. shiftMask, xK_space ), setLayout $ XMonad.layoutHook conf)
     , ((modm,               xK_n     ), refresh)
     , ((modm,               xK_Tab   ), windows W.focusDown)
     , ((modm,               xK_j     ), windows W.focusDown)
-    , ((modm,               xK_k     ), windows W.focusUp  )
-    , ((modm,               xK_m     ), windows W.focusMaster  )
-    , ((modm,               xK_Return), windows W.swapMaster)
-    , ((modm .|. shiftMask, xK_j     ), windows W.swapDown  )
-    , ((modm .|. shiftMask, xK_k     ), windows W.swapUp    )
+    , ((modm,               xK_k     ), windows W.focusUp)
+    , ((modm .|. shiftMask, xK_j     ), windows W.swapDown)
+    , ((modm .|. shiftMask, xK_k     ), windows W.swapUp)
+
     , ((modm,               xK_h     ), sendMessage Shrink)
     , ((modm,               xK_l     ), sendMessage Expand)
-    , ((modm,               xK_t     ), withFocused $ windows . W.sink)
-    , ((modm              , xK_comma ), sendMessage (IncMasterN 1))
-    , ((modm              , xK_period), sendMessage (IncMasterN (-1)))
- 
-    -- Toggle the status bar gap
-    -- Use this binding with avoidStruts from Hooks.ManageDocks.
-    -- See also the statusBar function from Hooks.DynamicLog.
-    --
-    -- , ((modm              , xK_b     ), sendMessage ToggleStruts)
-    , ((modm .|. shiftMask, xK_q     ), io (exitWith ExitSuccess))
-    , ((modm              , xK_q     ), spawn "xmonad --recompile; xmonad --restart")
-    ]
+    , ((modm,               xK_comma ), sendMessage (IncMasterN 1))
+    , ((modm,               xK_period), sendMessage (IncMasterN (-1)))
     
+    , ((modm,               xK_m     ), windows W.focusMaster)
+    , ((modm,               xK_Return), windows W.swapMaster)
+    , ((modm,               xK_t     ), withFocused $ windows . W.sink)]
+
+
     ++
-     
+
+    -- Switch between workspaces
+    [((m .|. modm, k), windows $ f i)
+    | (i, k) <- zip (XMonad.workspaces conf) [xK_1 .. xK_9]
+    , (f, m) <- [(W.greedyView, 0), (W.shift, shiftMask)]]
+
+    ++
+
     -- Switch between Screens
-    [ ((modm,                xK_p), prevScreen)
-    , ((modm,                xK_n), nextScreen)
-    , ((modm .|. shiftMask,  xK_p), shiftPrevScreen)
-    , ((modm .|. shiftMask,  xK_n), shiftNextScreen)
-    , ((modm,                xK_s), swapNextScreen)]
+    [ ((modm,               xK_p), prevScreen)
+    , ((modm,               xK_n), nextScreen)
+    , ((modm .|. shiftMask, xK_p), shiftPrevScreen)
+    , ((modm .|. shiftMask, xK_n), shiftNextScreen)
+    , ((modm,               xK_s), swapNextScreen)]
     
     ++
 
-    [((m .|. modm, k), windows $ f i)
-        | (i, k) <- zip (XMonad.workspaces conf) [xK_1 .. xK_9]
-        , (f, m) <- [(W.greedyView, 0), (W.shift, shiftMask)]]
-    ++
- 
     --
     -- mod-{w,e,r}, Switch to physical/Xinerama screens 1, 2, or 3
     -- mod-shift-{w,e,r}, Move client to screen 1, 2, or 3
     --
     [((m .|. modm, key), screenWorkspace sc >>= flip whenJust (windows . f))
-        | (key, sc) <- zip [xK_w, xK_e, xK_r] [0..]
-        , (f, m) <- [(W.view, 0), (W.shift, shiftMask)]]
+    | (key, sc) <- zip [xK_w, xK_e, xK_r] [0..]
+    , (f, m) <- [(W.view, 0), (W.shift, shiftMask)]]
+
+    ++
+
+    -- Toggle the status bar gap
+    -- Use this binding with avoidStruts from Hooks.ManageDocks.
+    -- See also the statusBar function from Hooks.DynamicLog.
+    --
+    -- , ((modm              , xK_b     ), sendMessage ToggleStruts)
+    [ ((modm .|. shiftMask, xK_q), io (exitWith ExitSuccess))
+    , ((modm,               xK_q), spawn "xmonad --recompile; xmonad --restart")]
+
     ++
     
     -- Application
-    [ ((modm .|. controlMask, xK_e     ), spawn "emacsclient -c -a \"\"")
-    , ((modm .|. controlMask, xK_w     ), spawn "chromium")
-    , ((modm .|. controlMask, xK_p     ), spawn "apvlv")      
-    , ((modm .|. controlMask, xK_m     ), spawn "evolution")
-    , ((modm .|. controlMask, xK_u     ), spawn "muliclient-x2go")
-    , ((modm .|. controlMask, xK_i     ), spawn "urxvt -e freetalk")]
+    [ ((modm .|. controlMask, xK_e), spawn "emacsclient -c -a \"\"")
+    , ((modm .|. controlMask, xK_w), spawn "chromium")
+    , ((modm .|. controlMask, xK_p), spawn "apvlv")
+    , ((modm .|. controlMask, xK_m), spawn "evolution")
+    , ((modm .|. controlMask, xK_u), spawn "muliclient-x2go")
+    , ((modm .|. controlMask, xK_i), spawn "urxvt -e freetalk")]
 
