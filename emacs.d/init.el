16b30ce6d633be4e367afa6a87c1735fbb84c1b0
Max Z. Chao <max.z.chao@gmail.com>
2012-10-03 21:22:00 +1000

    emacs/ecb: change the layout and size of ecb

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -91,7 +91,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(ecb-layout-window-sizes (quote (("mzc" (ecb-methods-buffer-name 0.21800947867298578 . 0.9824561403508771) (ecb-sources-buffer-name 0.22274881516587677 . 0.9824561403508771)))))
+ '(ecb-layout-window-sizes (quote (("mzc" (ecb-methods-buffer-name 0.2 . 0.975) (ecb-directories-buffer-name 0.19 . 0.35) (ecb-sources-buffer-name 0.19 . 0.625)))))
  '(safe-local-variable-values (quote ((c-indentation-style . bsd)))))
 				   
 (custom-set-faces
--- emacs.d/mzc-ecb.el
+++ emacs.d/mzc-ecb.el
@@ -11,4 +11,5 @@
 ;; Jump between ecb windows
 (global-set-key (kbd "C-c e") 'ecb-goto-window-edit-by-smart-selection)
 (global-set-key (kbd "C-c m") 'ecb-goto-window-methods)
-(global-set-key (kbd "C-c d") 'ecb-goto-window-sources)
+(global-set-key (kbd "C-c d") 'ecb-goto-window-directories)
+(global-set-key (kbd "C-c f") 'ecb-goto-window-sources)
