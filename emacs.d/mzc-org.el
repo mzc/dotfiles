65d543aa50dcb46263de21532d0e5212508b2445
Zhen (Max) Chao <max.z.chao@gmail.com>
2012-10-03 13:37:43 +1000

    emacs/org: add key-binding

--- emacs.d/mzc-org.el
+++ emacs.d/mzc-org.el
@@ -1,12 +1,29 @@
 (setq org-directory "~/Dropbox/org/")
 
+;; for org-member
+(org-remember-insinuate)
+(setq org-default-notes-file (concat org-directory "todo/1.idea.org"))
+(global-set-key (kbd "\C-cor") 'org-remember)
+
 ;; for todo
-(setq org-agenda-files (list "~/Dropbox/org/todo"))
+(setq todo-path "~/Dropbox/org/todo")
+(setq org-agenda-files (list todo-path))
 
 (setq org-todo-keywords
       '((sequence "TODO(t)" "DOING(i!)" "HANGUP(h@/!)" "|" "DONE(d@/!)" "CANCEL(c@/!)")))
 
-;; for org-member
-(org-remember-insinuate)
-(setq org-default-notes-file (concat org-directory "todo/1.ideas.org"))
-(global-set-key (kbd "C-c \C-r") 'org-remember)
+(defun do-gtd (name)
+  (interactive "sGTD: ")
+  (let* ((gtd-table '(("idea"   . "1.idea.org"  )
+		      ("task"   . "2a.task.org"  )
+		      ("maybe"  . "2b.maybe.org" )
+		      ("proj"   . "3.proj.org"   )
+		      ("finish" . "4a.finish.org")
+		      ("trash"  . "4b.trash.org" )))
+	 (gtd-name (assoc-default name gtd-table))
+	 (gtd-path (format "%s/%s" todo-path gtd-name)))
+    (if (stringp gtd-name)
+	(find-file gtd-path)
+      (message "Can't find \"%s\"" name))))
+
+(global-set-key (kbd "\C-cog") 'do-gtd)
