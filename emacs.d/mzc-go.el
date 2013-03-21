75bd99348cc87732123da60bcd6c3df56ace6bc5
Zhen (Max) Chao <max.z.chao@gmail.com>
2013-03-21 17:16:56 +1100

    emacs: improve go mode

--- emacs.d/mzc-go.el
+++ emacs.d/mzc-go.el
@@ -1,2 +1,17 @@
 (add-to-list 'load-path "~/repo/go/misc/emacs" t)
 (require 'go-mode-load)
+
+(add-hook 'before-save-hook 'gofmt-before-save)
+
+;; managing imports
+(add-hook 'go-mode-hook (lambda ()
+			  (local-set-key (kdb "C-c C-r")
+					 'go-remove-unused-imports)))
+(add-hook 'go-mode-hook (lambda ()
+			  (local-set-key (kdb "C-c i")
+					 'go-goto-imports)))
+
+;; default bind C-c C-d to godef-descibe
+(add-hook 'go-mode-hook (lambda ()
+			  (local-set-key (kbd \"M-.\")
+					 'godef-jump)))
