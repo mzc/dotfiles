68eb40675aec516691e1f534a541f666ca130fa8
Zhen (Max) Chao <max.z.chao@gmail.com>
2014-06-03 14:35:17 +1000

    emacs/my-go: new flymake, autocomplete, eldoc config

--- emacs.d/my-go.el
+++ emacs.d/my-go.el
@@ -1,16 +1,35 @@
 (require 'go-mode-load)
 
+;;; Invoke gofmt before save
 (add-hook 'before-save-hook 'gofmt-before-save)
 
-;; managing imports
+;;; Managing imports
+;; "C-c C-a" go-import-add
 (add-hook 'go-mode-hook (lambda ()
-			  (local-set-key (kdb "C-c C-r")
-					 'go-remove-unused-imports)))
+			  (local-set-key (kdb "C-c C-r") 'go-remove-unused-imports)))
 (add-hook 'go-mode-hook (lambda ()
-			  (local-set-key (kdb "C-c i")
-					 'go-goto-imports)))
+			  (local-set-key (kdb "C-c i") 'go-goto-imports)))
 
-;; default bind C-c C-d to godef-descibe
+;;; Navigating code
+;; "C-M-a"   beginning-of-defun
+;; "C-M-e"   end-of-defun
+;; "C-c C-d" godef-descibe
 (add-hook 'go-mode-hook (lambda ()
-			  (local-set-key (kbd \"M-.\")
-					 'godef-jump)))
+			  (local-set-key (kbd \"M-.\") 'godef-jump)))
+
+;;; Syntax checking
+(add-to-list 'load-path (concat (getenv "GOPATH") "/src/github.com/dougm/goflymake"))
+(require 'go-flymake)
+(require 'go-flycheck)
+
+;;; Autocompletion
+(add-to-list 'load-path (concat (getenv "GOPATH") "/src/github.com/nsf/gocode/emacs"))
+(require 'go-autocomplete)
+(require 'auto-complete-config)
+
+;;; Show the argument list on echo area
+(require 'go-eldoc)
+(add-hook 'go-mode-hook 'go-eldoc-setup)
+(set-face-attribute 'eldoc-highlight-function-argument nil
+		    :underline t :foreground "gree"
+		    :weight 'bold)
