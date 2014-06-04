8976422449816545dfb63f1294796e92083abeec
Zhen (Max) Chao <max.z.chao@gmail.com>
2014-06-04 15:07:34 +1000

    emacs/cedet: cleanup

--- emacs.d/my-cedet.el
+++ emacs.d/my-cedet.el
@@ -1,44 +1,27 @@
 (require 'cedet)
 
-(setq semantic-default-submodes '(;; caches the results of parsing source code files
-				  global-semanticdb-minor-mode
+;; Caches the results of parsing source code files
+(add-to-list 'semantic-default-submodes 'global-semanticdb-minor-mode)
 
-				  ;; perform buffer parsing during idle time
-				  global-semantic-idle-scheduler-mode
+;; Perform buffer parsing during idle time
+(add-to-list 'semantic-default-submodes 'global-semantic-idle-scheduler-mode)
 
-				  ;; displays a short summary of the symbol at point
-                                  global-semantic-idle-summary-mode
+;; Highlights local names that are the same as of tag under cursor;
+(add-to-list 'semantic-default-submodes 'global-semantic-idle-local-symbol-highlight-mode)
 
-				  ;; perform code completions during idle time
-				  global-semantic-idle-completions-mode
+;; Highlights the declaration line of the current function
+(add-to-list 'semantic-default-submodes 'global-semantic-highlight-func-mode)
 
-				  ;; keeps track of the tags have been edited
-                                  global-semantic-mru-bookmark-mode
+;; Displays a header line that shows the declaration line of the function
+(add-to-list 'semantic-default-submodes 'global-semantic-stickyfunc-mode)
 
-				  ;; highlights the declaration line of the current function
-				  global-semantic-highlight-func-mode
-
-				  ;; displays a header line that shows the declaration line of the function				  
-				  global-semantic-stickyfunc-mode
-				  ))
 (semantic-mode 1)
 
-;; highlight the effected text when editing
+;; Highlights the effected text when editing
 (global-semantic-highlight-edits-mode)
 
-;; highlight unmatched-syntax tokens
+;; Highlights unmatched-syntax tokens
 (global-semantic-show-unmatched-syntax-mode 1)
 
-;; display parser cache state in the modeline
+;; Displays parser cache state in the modeline
 (global-semantic-show-parser-state-mode 1)
-
-;; (setq semanticdb-project-roots (list (expand-file-name "/")))
-(defconst cedet-user-include-dirs
-  (list ".." "../include" "../inc" "../common" "../public"
-        "../.." "../../include" "../../inc" "../../common" "../../public"))
-(require 'semantic-c nil 'noerror)
-(let ((include-dirs cedet-user-include-dirs))
-  (mapc (lambda (dir)
-          (semantic-add-system-include dir 'c++-mode)
-          (semantic-add-system-include dir 'c-mode))
-        include-dirs))
